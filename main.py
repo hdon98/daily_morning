485d98e35fa1ca21f0bc8cb2cbd4c89840dbc6b4
Dong <570843971@qq.com>
2022-08-25 01:00:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -100,12 +100,12 @@ data = {"today": {"value": today.strftime("%Y-%m-%d")},
         "words": {"value": get_words(), "color": get_random_color()},
         "comf": {"value": "舒适指数：『" + comf['brf'] + "』 " + comf['txt'], "color": "#00FF00"},
         "drsg": {"value": "穿衣指数：『" + drsg['brf'] + "』 " + drsg['txt'], "color": "#FF7F00"},
-        "flu": {"value": "流感指数：『" + flu['brf'] + "』 " + flu['txt'], "color": "#FFFF00"}}
-        # "sport": {"value": "运动指数：『" + sport['brf'] + "』 " + sport['txt'], "color": "#00FF00"},
-        # "trav": {"value": "出行指数：『" + trav['brf'] + "』 " + trav['txt'], "color": "#00FFFF"},
-        # "uv": {"value": "辐射指数：『" + uv['brf'] + "』 " + uv['txt'], "color": "#0000FF"},
-        # "air": {"value": "空气指数：『" + air['brf'] + "』 " + air['txt'], "color": "#8B00FF"},
-
+        "flu": {"value": "流感指数：『" + flu['brf'] + "』 " + flu['txt'], "color": "#FFFF00"},
+        "sport": {"value": "运动指数：『" + sport['brf'] + "』 " + sport['txt'], "color": "#00FF00"},
+        "trav": {"value": "出行指数：『" + trav['brf'] + "』 " + trav['txt'], "color": "#00FFFF"},
+        "uv": {"value": "辐射指数：『" + uv['brf'] + "』 " + uv['txt'], "color": "#0000FF"},
+        "air": {"value": "空气指数：『" + air['brf'] + "』 " + air['txt'], "color": "#8B00FF"},
+        }
 
 user_ids = user_id.split(",")
 for i in user_ids:
